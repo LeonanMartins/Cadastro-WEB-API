92a6079439ce9e89ff607e680d5b57184c918faf
Leonan Martins Rodrigues Junior <65869394+LeonanMartins@users.noreply.github.com>
2022-05-10 19:20:52 +0100

    Add files via upload

---
+++ Data/ClienteContext.cs
@@ -0,0 +1,20 @@
+﻿using Cadastro.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cadastro.Data
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var cliente = modelBuilder.Entity<Cliente>();
+            cliente.ToTable("Clientes");
+            cliente.HasKey(x => x.Id);
+        }
+    }
+}
