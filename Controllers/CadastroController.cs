2f4c9ce0631cb61f3dc0fb395c4513de4e3adb70
Leonan Martins Rodrigues Junior <65869394+LeonanMartins@users.noreply.github.com>
2022-05-10 19:20:25 +0100

    Add files via upload

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,32 @@
+﻿using Cadastro.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CadastroController : ControllerBase
+    {
+        private static List<Cliente> Clientes()
+        {
+            return new List<Cliente>
+            {
+                new Cliente{ Nome = "Lucas", Id = 1, DataContacto = new DateTime(1998, 10,5)}
+            };
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Clientes());
+        }
+
+        [HttpPost]
+        public IActionResult Post(Cliente cliente)
+        {
+            var clientes = Clientes();
+            clientes.Add(cliente);
+            return Ok(clientes);
+        }
+
+    }
+}
