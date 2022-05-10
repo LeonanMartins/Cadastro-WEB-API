192d19ade64f0bd1ce640d2679804237c31e0f27
Leonan Martins Rodrigues Junior <65869394+LeonanMartins@users.noreply.github.com>
2022-05-10 19:19:47 +0100

    Add files via upload
    Projeto Web API desenvolvido em .NET 6, que tem como objetivo ser uma aplicação que alimenta e é alimentada por um sistema de contatos dos clientes e que também, fará controle de contatos dos mesmos. Implementei o banco de dados e para isso precisei utilizar de SQL Server, Entity Framework e Migrations.

---
+++ Model/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace Cadastro.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string TipoFormacao { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataContacto { get; set; }
+        public string HorarioContacto { get; set; }
+        public bool Retorno { get; set; }
+        public int NumeroContactos { get; set; }
+        public DateTime PrimeiroContacto { get; set; }
+        public DateTime UltimoContacto { get; set; }
+        public DateTime Contacto { get; set; }
+        public string Obs { get; set; }
+
+
+
+
+    }
+}
